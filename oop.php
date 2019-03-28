432361c824879544fd159749c8da07cd26fd9fc0
Manvitha <meetmanvitha@gmail.com>
2019-03-28 11:07:50 +0530

    Update oop.php

--- oop.php
+++ oop.php
@@ -1,16 +1,3 @@
-<html>
-<body>
- /* <?php
-  include("product.php");
-  //$GET[];
-  $product1= new Product;
-  if (isset($_POST['submit']))
-  {
-  $product1->setId($_POST['productId']);
-  $product1->setName($_POST['productName']);
-  $product1->display();
-  }
-?>*/
 <?php
 include("product.php");
 $product1= new Product;
@@ -24,10 +11,3 @@ $product1->display();
 echo $x. "-" . $y;
 }
 ?>
-
-
-</body>
-
-
-
-</html>
