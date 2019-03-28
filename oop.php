8e1f52afe69328b338deeaf8459bdc76ef6a3086
Manvitha <meetmanvitha@gmail.com>
2019-03-28 10:49:26 +0530

    Update oop.php

--- oop.php
+++ oop.php
@@ -12,10 +12,15 @@
   }
 ?>*/
 <?php
+include("product.php");
+$product1= new Product;
 if (isset($_POST['submit']))
 {
 $x=$_POST['productId'];
 $y=$_POST['productName'];
+$product1->setId($_POST['productId']);
+$product1->setName($_POST['productName']);
+$product1->display();
 echo $x. "-" . $y;
 }
 ?>
